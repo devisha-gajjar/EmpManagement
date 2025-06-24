44e19d1eb65f0c4a15de7ee7d96d1ca7b3b5f9af
devisha-gajjar <tatva.pctr45@outlook.com>
2025-06-24 19:03:30 +0530

    commit

--- employee-management-app/src/main.ts
+++ employee-management-app/src/main.ts
@@ -6,10 +6,28 @@ import { routes } from './app/app.routes';
 import { authInterceptor } from './app/interceptor/auth-interceptor';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideToastr } from 'ngx-toastr';
+import { environment } from '../src/environments/environment';
+import { SocialLoginModule, SocialAuthServiceConfig, GoogleLoginProvider } from '@abacritt/angularx-social-login';
+import { importProvidersFrom } from '@angular/core';
+
 
 bootstrapApplication(AppComponent, {
   providers: [provideRouter(routes),
   provideHttpClient(withInterceptors([authInterceptor])),
   provideAnimations(),
-  provideToastr()]
+  provideToastr(),
+  importProvidersFrom(SocialLoginModule),
+  {
+    provide: 'SocialAuthServiceConfig',
+    useValue: {
+      autoLogin: false,
+      providers: [
+        {
+          id: GoogleLoginProvider.PROVIDER_ID,
+          provider: new GoogleLoginProvider(environment.googleClientId),
+        },
+      ],
+      onError: (err: any) => console.error(err),
+    } as SocialAuthServiceConfig,
+  }]
 }).catch(err => console.error(err));
